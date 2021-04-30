0af8f0353cef5d356792c23db4da09eba9a45289
alygreening <>
2021-04-30 09:04:53 +0000

    added print name

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,8 @@
 def main():
-    #write your code below this line
+  name = input()
+  # this should be name = input()
+  # yours isn't wrong (as in it works), it's just not what the README asked for :)
+  print ("Hi " + name)
 
 if __name__ == '__main__':
     main()
